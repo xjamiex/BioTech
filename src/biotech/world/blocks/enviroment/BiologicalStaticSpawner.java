45f806abd7960e535788905b837d7cc845f57c97
jammuu <panglexiao@gmail.com>
2024-12-20 11:00:47 -0800

    gr

--- src/biotech/world/blocks/enviroment/BiologicalStaticSpawner.java
+++ src/biotech/world/blocks/enviroment/BiologicalStaticSpawner.java
@@ -6,19 +6,18 @@ import arc.math.Mathf;
 import biotech.content.BioUnits;
 import mindustry.Vars;
 import mindustry.content.Fx;
+import mindustry.entities.Damage;
 import mindustry.entities.Effect;
 import mindustry.game.EventType;
 import mindustry.game.Team;
-import mindustry.gen.Bullet;
-import mindustry.gen.Call;
-import mindustry.gen.Sounds;
-import mindustry.gen.Unit;
+import mindustry.gen.*;
 import mindustry.type.UnitType;
+import mindustry.world.blocks.environment.Prop;
 import mindustry.world.blocks.payloads.UnitPayload;
 import mindustry.world.blocks.units.UnitBlock;
 import mindustry.world.blocks.units.UnitFactory;
 
-public class BiologicalStaticSpawner extends UnitBlock {
+public class BiologicalStaticSpawner extends Prop {
 
     public static UnitType spawnedUnit = BioUnits.strider;
     public static float spawnTime = 700;
@@ -42,39 +41,7 @@ public class BiologicalStaticSpawner extends UnitBlock {
         breakable = false;
     }
 
-    public class BiologicalStaticSpawnerBuild extends UnitBuild {
-
-        @Override
-        public void updateTile() {
-            super.updateTile();
-            if(efficiency > 0){
-                time += edelta() * speedScl * Vars.state.rules.unitBuildSpeed(team);
-                progress += edelta() * Vars.state.rules.unitBuildSpeed(team);
-                speedScl = Mathf.lerpDelta(speedScl, 1f, 0.05f);
-            }else{
-                speedScl = Mathf.lerpDelta(speedScl, 0f, 0.05f);
-            }
-
-            if(progress >= spawnTime){
-                progress %= 1f;
-
-                for (int i = 0; i < spawnAmount + Mathf.random(-5, 5); i++) {
-                    Unit unit = spawnedUnit.spawn(Team.crux, tile.worldx(), tile.worldy());
-                    unit.speedMultiplier = Mathf.randomSeed(unit.id, -0.5f, 1.5f);
-                    payload = new UnitPayload(unit);
-                    payVector.setZero();
-                    Events.fire(new EventType.UnitCreateEvent(payload.unit, this));
-
-                    effect.at(x, y);
-
-                    if (payload.dump()) {
-                        Call.unitBlockSpawn(tile);
-                    }
-                }
-            }
-
-            progress = Mathf.clamp(progress, 0, spawnTime);
-        }
+    public class BiologicalStaticSpawnerBuild extends Building {
 
         @Override
         public void draw() {

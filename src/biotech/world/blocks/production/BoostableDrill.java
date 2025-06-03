2d96b7ec075f5f6c3ca241b850d839eac8976371
jammuu <panglexiao@gmail.com>
2025-06-02 21:57:13 -0700

    bloat

--- src/biotech/world/blocks/production/BoostableDrill.java
+++ src/biotech/world/blocks/production/BoostableDrill.java
@@ -39,47 +39,4 @@ public class BoostableDrill extends Drill {
         }
         return boost;
     }
-
-    public class BoostableDrillBuild extends DrillBuild {
-
-        @Override
-        public void updateTile() {
-            countOre(tile);
-            if(timer(timerDump, dumpTime)) {
-                dump(dominantItem != null && items.has(dominantItem) ? dominantItem : null);
-            }
-            Log.info(dominantItems + " " + efficiency);
-
-            if(dominantItem == null) {
-                return;
-            }
-
-            timeDrilled += warmup * delta();
-
-            float delay = getDrillTime(dominantItem);
-
-            if(items.total() < itemCapacity && dominantItems > 0 && efficiency > 0){
-                float speed = Mathf.lerp(1f, liquidBoostIntensity, optionalEfficiency) * efficiency;
-
-                lastDrillSpeed = (speed * dominantItems * warmup) / delay;
-                warmup = Mathf.approachDelta(warmup, speed, warmupSpeed);
-                progress += delta() * dominantItems * speed * warmup;
-
-                if(Mathf.chanceDelta(updateEffectChance * warmup))
-                    updateEffect.at(x + Mathf.range(size * 2f), y + Mathf.range(size * 2f));
-            }else{
-                lastDrillSpeed = 0f;
-                warmup = Mathf.approachDelta(warmup, 0f, warmupSpeed);
-                return;
-            }
-
-            if(dominantItems > 0 && progress >= delay && items.total() < itemCapacity){
-                offload(dominantItem);
-
-                progress %= delay;
-
-                if(wasVisible && Mathf.chanceDelta(updateEffectChance * warmup)) drillEffect.at(x + Mathf.range(drillEffectRnd), y + Mathf.range(drillEffectRnd), dominantItem.color);
-            }
-        }
-    }
 }
